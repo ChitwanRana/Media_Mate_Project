04ff921de1f33f7d5d02fc6ea018e6116ba52d66
ChitwanRana <kushr4446@gmail.com>
2024-11-06 22:48:27 +0530

    Modification

--- MediaMate/movies/views.py
+++ MediaMate/movies/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pickle
+import pandas as pd
 # Create your views here.
+
+
+
+
 def home(request):
      return render(request,'home.html')
 
 def movies(request):
-     return render(request,'movies.html')
+     movies_dict=pickle.load(open('movies/pklfile_movie/movies_dict.pkl','rb'))
+     movies=pd.DataFrame(movies_dict)
+     movie_titles=movies['title'].values
+
+
+     if request.method == 'POST':
+        # Get the user input from the form
+        user_input = request.POST.get('movie_input')
+
+        
+     return render(request, 'movies.html', {'movie_titles': movie_titles})
 
 def About(request):
      return render(request,'about.html')

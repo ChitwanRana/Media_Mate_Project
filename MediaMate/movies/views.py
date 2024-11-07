68b5a0a5dd7aaada171ddd297466b3cee359c77f
ChitwanRana <kushr4446@gmail.com>
2024-11-07 17:42:41 +0530

    Movie Recommend System Completed

--- MediaMate/movies/views.py
+++ MediaMate/movies/views.py
@@ -11,17 +11,27 @@ def home(request):
      return render(request,'home.html')
 
 def movies(request):
-     movies_dict=pickle.load(open('movies/pklfile_movie/movies_dict.pkl','rb'))
-     movies=pd.DataFrame(movies_dict)
-     movie_titles=movies['title'].values
+    movies_dict = pickle.load(open('movies/pklfile_movie/movies_dict.pkl', 'rb'))
+    movies = pd.DataFrame(movies_dict)
+    movie_titles = movies['title'].values
+    similar = pickle.load(open('movies/pklfile_movie/similar.pkl', 'rb'))
 
+    recommended_movies = []  # Initialize an empty list for recommended movies
 
-     if request.method == 'POST':
+    if request.method == 'POST':
         # Get the user input from the form
         user_input = request.POST.get('movie_input')
 
-        
-     return render(request, 'movies.html', {'movie_titles': movie_titles})
+        index = movies[movies['title'] == user_input].index[0]
+        distance = similar[index]
+        movies_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])[1:6]
+
+        recommended_movies = []
+        for i in movies_list:
+            recommended_movies.append(movies.iloc[i[0]]['title'])
+
+    return render(request, 'movies.html', {'movie_titles': movie_titles, 'recommended_movies': recommended_movies})
+
 
 def About(request):
      return render(request,'about.html')

51e1f009635d15433115fc30bffa57bc3f4428b7
ChitwanRana <kushr4446@gmail.com>
2024-11-08 00:28:37 +0530

    Deploy All Recommend System

--- MediaMate/books/views.py
+++ MediaMate/books/views.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 popular_df=pickle.load(open('books/pklfiles/popular.pkl','rb'))
 pt=pickle.load(open('books/pklfiles/pt.pkl','rb'))
-books=pickle.load(open('books/pklfiles/books.pkl','rb'))
+books_data = pickle.load(open('books/pklfiles/books.pkl', 'rb'))
 similarity_scores=pickle.load(open('books/pklfiles/similarity_scores.pkl','rb'))
 
 # Create your views here.
@@ -24,38 +24,32 @@ def books(request):
     
     return render(request, 'books.html', {'books': books_data})
 
-def recommend(request): 
+def recommend(request):
     if request.method == 'POST':
-        # Get the user input from the form
         book_name = request.POST.get('user_input')
-        
-        # Debug pt type
-        print(f"pt type: {type(pt)}")
 
-        if isinstance(pt, (pd.DataFrame, pd.Series)):
-            # Ensure pt.index is accessible
-            try:
-                index = np.where(pt.index == book_name)[0][0]
-            except IndexError:
-                return render(request, 'book_recommend.html', {'error': 'Book not found.'})
-        else:
-            return render(request, 'book_recommend.html', {'error': 'Pivot table (pt) is not defined properly.'})
+        try:
+            # Assuming pt is defined somewhere in your code
+            index = np.where(pt.index == book_name)[0][0]  # Find the index of the book
+        except IndexError:
+            return render(request, 'book_recommend.html', {'error': 'Book not found'})
 
-        # Proceed with the rest of the recommendation logic
-        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
-        
+        # Ensure similarity_scores is defined and valid
+        try:
+            similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+        except IndexError:
+            return render(request, 'book_recommend.html', {'error': 'Invalid index for similarity scores'})
+
         data = []
         for i in similar_items:
             item = []
-            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+            temp_df = books_data[books_data['Book-Title'] == pt.index[i[0]]]
             item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
             item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
             item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
-            
+
             data.append(item)
-        
+
         return render(request, 'book_recommend.html', {'data': data})
-    
+
     return render(request, 'book_recommend.html')
-
-
